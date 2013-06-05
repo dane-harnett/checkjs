318b63bc1cfcfb9c512c3aebe2f2732347ace4fb
Dane Harnett <dane_harnett@flightcentre.com>
2013-06-05 21:11:57 +1000

    Updated examples.

--- check-example.js
+++ check-example.js
@@ -6,12 +6,57 @@
     }
   });
 
-  var x = check('x');
-  var y = check('y');
+  var xchk = check('x');
+  var ychk = check('y');
 
-  x.log(); // should log 'x'
+  xchk.log(); // should log 'x'
 
-  x.equals('x').log(); // should log 'x'
-  x.equals('y').log(); // should not log
+  xchk.equals('x').log(); // should log 'x'
+  xchk.equals('y').log(); // should not log
+
+
+  // PROCEDURAL EXAMPLES AND CHECKJS ALTERNATIVES
+
+  var x = 'x';
+  var y = 'y';
+
+
+  // example 1
+  if (typeof x !== 'undefined') {
+    doSomething();
+  }
+  check(x).defined().chain(doSomething);
+
+  // example 2
+  if (typeof x !== 'undefined' && x !== '') {
+    doSomething();
+  }
+  check(x).defined().notEmptyString().chain(doSomething);
+
+
+  // example 3
+  if (typeof x !== 'undefined') {
+    doSomething();
+  } else {
+    doSomethingElse();
+  }
+  check(x).defined().chain(doSomething).otherwise(doSomethingElse);
+
+
+  // example 4
+  if (typeof x !== 'undefined' && typeof y !== 'undefined') {
+    doSomething();
+  }
+  // possible options
+  check(x).defined().pass(y).defined().chain(doSomething);
+  check(x).defined().check(y).defined().chain(doSomething);
+  check(x).defined().and(y).defined().chain(doSomething);
+
+  // example 5
+  if (typeof x !== 'undefined' || typeof y !== 'undefined') {
+    doSomething();
+  }
+  // possible options
+  check(x).defined().or(y).defined().chain(doSomething);
 
 })();
